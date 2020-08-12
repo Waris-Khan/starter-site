75d8a0fe76f5bf1bfff0ee9548f084333e628ccf
Waris-Khan <k.waris14@gmail.com>
2020-08-12 11:02:07 +0500

    Updates

---
+++ precache-manifest.6b99a4f6ca643cb1d5b8f8b2d3a289ab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956270a460e50b47fbf4fde261ae7c44",
+    "url": "/starter-site/index.html"
+  },
+  {
+    "revision": "ecc98acc086cd1e76d24",
+    "url": "/starter-site/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f319af4e8ae83b9d25df",
+    "url": "/starter-site/static/css/main.2efa5941.chunk.css"
+  },
+  {
+    "revision": "ecc98acc086cd1e76d24",
+    "url": "/starter-site/static/js/2.f18a3cbf.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/starter-site/static/js/2.f18a3cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f319af4e8ae83b9d25df",
+    "url": "/starter-site/static/js/main.f78d006f.chunk.js"
+  },
+  {
+    "revision": "2e1f0ad8db659d29337f",
+    "url": "/starter-site/static/js/runtime-main.19ce6860.js"
+  },
+  {
+    "revision": "9063b71c6b765ec3f1c57b8c4fa76251",
+    "url": "/starter-site/static/media/about-us.9063b71c.png"
+  },
+  {
+    "revision": "7cbeed91ed3ba49369c6944ea3047018",
+    "url": "/starter-site/static/media/hh.7cbeed91.png"
+  },
+  {
+    "revision": "1f4ddf3054f21d3e8d15625cb5d4d88e",
+    "url": "/starter-site/static/media/logo-design.1f4ddf30.png"
+  },
+  {
+    "revision": "dd97288d5527fb0f76b5bba75675ddf6",
+    "url": "/starter-site/static/media/marketing.dd97288d.png"
+  },
+  {
+    "revision": "8f4235085b7b0eab3c46066fc4ca81f6",
+    "url": "/starter-site/static/media/mob-dev.8f423508.png"
+  },
+  {
+    "revision": "cafb2246e24b2203ed91c593ffd36cb7",
+    "url": "/starter-site/static/media/seo.cafb2246.png"
+  },
+  {
+    "revision": "05dd624fbadb1dd52a2b3ad55e6be37e",
+    "url": "/starter-site/static/media/software-dev.05dd624f.png"
+  },
+  {
+    "revision": "f4342dc6d10812df18e07c039c8420d0",
+    "url": "/starter-site/static/media/web-dev.f4342dc6.png"
+  }
+]);
